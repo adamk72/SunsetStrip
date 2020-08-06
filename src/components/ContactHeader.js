56f61ec115e535d83a98109887da68af225a4f4e
Adam K <adam@kecskes.net>
2020-08-06 15:11:29 -0500

    Got icons working, finally

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -4,8 +4,11 @@ import cx from 'classnames'
 
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-
 import { makeStyles } from '@material-ui/core/styles';
+import Instagram from '@material-ui/icons/Instagram';
+import Phone from '@material-ui/icons/Phone';
+import Facebook from '@material-ui/icons/Facebook';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'flex-start',
         marginBottom: theme.spacing('4'),
     },
+    navIcon: {
+        marginRight: '5px'
+    }
 }));
 
 const ContactHeader = ({ siteTitle }) => {
@@ -20,9 +26,11 @@ const ContactHeader = ({ siteTitle }) => {
     return (
         <div className={cx(classes.root, "container")}>
             <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-                <Button>One</Button>
-                <Button>Two</Button>
-                <Button>Three</Button>
+
+
+                <Button><Instagram className={classes.navIcon} />Instagram</Button>
+                <Button><Facebook className={classes.navIcon} />Facebook</Button>
+                <Button><Phone className={classes.navIcon} />Phone</Button>
             </ButtonGroup>
         </div>
     )
