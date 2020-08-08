52a3bbfdb17e44c4e14503d1abfd70ddc3bc7258
Adam K <adam@kecskes.net>
2020-08-07 21:45:04 -0500

    Taking out ALL icons to see what's up on Heroku

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -3,9 +3,9 @@ import React from "react"
 
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Instagram from '@material-ui/icons/Instagram';
-import Phone from '@material-ui/icons/Phone';
-import Facebook from '@material-ui/icons/Facebook';
+// import Instagram from '@material-ui/icons/Instagram';
+// import Phone from '@material-ui/icons/Phone';
+// import Facebook from '@material-ui/icons/Facebook';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -27,9 +27,9 @@ const ContactHeader = ({ siteTitle }) => {
         <div className={classes.root}>
             <div className="container">
                 <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" >
-                    <Button><Instagram className={classes.navIcon} />Instagram</Button>
+                    {/* <Button><Instagram className={classes.navIcon} />Instagram</Button>
                     <Button><Facebook className={classes.navIcon} />Facebook</Button>
-                    <Button><Phone className={classes.navIcon} />Phone</Button>
+                    <Button><Phone className={classes.navIcon} />Phone</Button> */}
                 </ButtonGroup>
             </div>
         </div>
