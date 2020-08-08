d7d9a5a0c16d24d4fb88c742bfe4c1fff3ffb710
Adam K <adam@kecskes.net>
2020-08-07 21:06:27 -0500

    Remove filesystem plugin since I don't use it and an error is popping up in Heroku push around it

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,13 @@ module.exports = {
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-json`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
