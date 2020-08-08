366170c3f4e958effceee3908e781bed22a05503
Adam K <adam@kecskes.net>
2020-08-07 20:48:59 -0500

    Get Gatsby to use theme provider and such correctly

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -28,8 +28,6 @@ export const wrapRootElement = ({ element }) => {
 
 
 export const wrapPageElement = ({ element, props }) => {
-    // props provide same data to Layout as Page element will get
-    // including location, data, etc - you don't need to pass it
     return <Layout {...props}>{element}</Layout>
 }
 
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,31 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
+import React from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Layout from './src/components/Layout'
+
+const theme = createMuiTheme({
+    spacing: factor => `${0.25 * factor}rem`,
+    palette: {
+        // primary: { main: '#663399' },
+        primary: { main: '#663399' }, // rebeccapurple
+        secondary: { main: '#23994A' } // Green
+    }
+})
+
+export const wrapRootElement = ({ element }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {element}
+        </ThemeProvider>
+    )
+}
+
+
+
+export const wrapPageElement = ({ element, props }) => {
+    return <Layout {...props}>{element}</Layout>
+}
+
 // You can delete this file if you're not using it
